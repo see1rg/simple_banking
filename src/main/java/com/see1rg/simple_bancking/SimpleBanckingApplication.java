ebdbd68e710bfc3ca617a9066062054cecdb64f8
see1rg <see1rg@mail.ru>
2023-10-05 15:19:03 +0300

    Initial commit

---
+++ src/main/java/com/see1rg/simple_bancking/SimpleBanckingApplication.java
@@ -0,0 +1,13 @@
+package com.see1rg.simple_bancking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBanckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBanckingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/see1rg/simple_bancking/SimpleBanckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.see1rg.simple_bancking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBanckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
